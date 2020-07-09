31f0a3a48dc0a4cfe9af28ad224c25684d72ec19
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-07-09 10:12:40 +0800

    Added toolbar to pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AppMaterialModule } from './app-material.module';
+
 import { AppComponent } from './app.component';
 import { pages } from './pages';
 import { components } from './components';
 
 @NgModule({
-    declarations: [AppComponent, ...pages, ...components],
-    imports: [BrowserModule, AppRoutingModule],
-    providers: [],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent, ...pages, ...components],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AppMaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,10 +1,13 @@
 export { FeatureTableComponent } from './feature-table/feature-table.component';
 export { FeatureGroupContainerComponent } from './feature-group-container/feature-group-container.component';
+export { NavbarComponent } from './navbar/navbar.component';
 
 import { FeatureTableComponent } from './feature-table/feature-table.component';
 import { FeatureGroupContainerComponent } from './feature-group-container/feature-group-container.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 export const components = [
-    FeatureTableComponent,
-    FeatureGroupContainerComponent,
+  FeatureTableComponent,
+  FeatureGroupContainerComponent,
+  NavbarComponent,
 ];
