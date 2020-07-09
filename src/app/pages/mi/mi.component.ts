3537485d58e4195c85378b8524ecdb8824cf67b4
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-07-09 23:54:36 +0800

    Mirrored stuff to mi and vancomycin

--- src/app/pages/mi/mi.component.ts
+++ src/app/pages/mi/mi.component.ts
@@ -1,4 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { List } from 'immutable';
+import { Observable } from 'rxjs';
+
+import { miFeatures } from '../../data';
+import { AppState, Mi } from '../../store';
 
 @Component({
   selector: 'app-mi',
@@ -6,4 +12,34 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./mi.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MiPage {}
+export class MiPage {
+  public readonly features = miFeatures;
+
+  public x$: Observable<List<List<number>>>;
+  public predictions$: Observable<List<number>>;
+  public weights$: Observable<List<List<number>>>;
+  public showPredictions$: Observable<boolean>;
+  public showWeights$: Observable<boolean>;
+  public isLoading$: Observable<boolean>;
+
+  constructor(private store: Store) {
+    this.x$ = this.store.select(AppState.miX);
+    this.predictions$ = this.store.select(AppState.miPredictions);
+    this.weights$ = this.store.select(AppState.miWeights);
+    this.showPredictions$ = this.store.select(AppState.showMiPredictions);
+    this.showWeights$ = this.store.select(AppState.showMiWeights);
+    this.isLoading$ = this.store.select(AppState.isMiLoading);
+  }
+
+  public onLoadSample() {
+    this.store.dispatch(new Mi.LoadSample());
+  }
+
+  public onReset() {
+    this.store.dispatch(new Mi.Reset());
+  }
+
+  public onPredict() {
+    this.store.dispatch(new Mi.Predict());
+  }
+}
--- src/app/pages/vancomycin/vancomycin.component.ts
+++ src/app/pages/vancomycin/vancomycin.component.ts
@@ -1,4 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { List } from 'immutable';
+import { Observable } from 'rxjs';
+
+import { vancomycinFeatures } from '../../data';
+import { AppState, Vancomycin } from '../../store';
 
 @Component({
   selector: 'app-vancomycin',
@@ -6,4 +12,36 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./vancomycin.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VancomycinPage {}
+export class VancomycinPage {
+  public readonly features = vancomycinFeatures;
+
+  public x$: Observable<List<List<number>>>;
+  public predictions$: Observable<List<number>>;
+  public weights$: Observable<List<List<number>>>;
+  public showPredictions$: Observable<boolean>;
+  public showWeights$: Observable<boolean>;
+  public isLoading$: Observable<boolean>;
+
+  constructor(private store: Store) {
+    this.x$ = this.store.select(AppState.vancomycinX);
+    this.predictions$ = this.store.select(AppState.vancomycinPredictions);
+    this.weights$ = this.store.select(AppState.vancomycinWeights);
+    this.showPredictions$ = this.store.select(
+      AppState.showVancomycinPredictions
+    );
+    this.showWeights$ = this.store.select(AppState.showVancomycinWeights);
+    this.isLoading$ = this.store.select(AppState.isVancomycinLoading);
+  }
+
+  public onLoadSample() {
+    this.store.dispatch(new Vancomycin.LoadSample());
+  }
+
+  public onReset() {
+    this.store.dispatch(new Vancomycin.Reset());
+  }
+
+  public onPredict() {
+    this.store.dispatch(new Vancomycin.Predict());
+  }
+}
