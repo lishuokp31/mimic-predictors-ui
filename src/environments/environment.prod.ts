fec785c20f5c1e199353c9e624d77b50f855e84d
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-11-04 11:05:35 +0800

    Changed API url to docker swarm cluster

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://1158269314454927.cn-beijing.fc.aliyuncs.com/2016-08-15/proxy/mimic-predictor'
+  apiUrl: 'http://114.116.207.63/api'
 };
