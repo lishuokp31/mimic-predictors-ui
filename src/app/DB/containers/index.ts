93d7606f7bd4652f9306e25f69341f5ef30ddc51
lishuokp31 <13541831470@163.com>
2021-03-16 00:18:17 +0800

    Revert "Revert "完成部分前端""
    This reverts commit d22057ebb188af10140471b7c872c339ee618b5c.

---
+++ src/app/DB/containers/index.ts
@@ -0,0 +1,8 @@
+export { DBComponent } from './DB/DB.component';
+
+import { DBComponent } from './DB/DB.component';
+
+
+export const containers = [
+  DBComponent
+];
---
+++ src/app/ner/components/index.ts
@@ -0,0 +1,13 @@
+// export { NerGetdataComponent } from './ner-getdata/ner-getdata.component';
+// export { NerPutresultComponent } from './ner-putresult/ner-putresult.component';
+
+// import { NerGetdataComponent } from './ner-getdata/ner-getdata.component';
+// import { NerPutresultComponent } from './ner-putresult/ner-putresult.component';
+
+// export const components = [
+//   NerGetdataComponent,
+//   NerPutresultComponent,
+// ];
+
+
+export const components = [];
